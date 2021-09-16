82ad081642329fec8ad2a96c74ee38180d146996
Ward Cunningham <ward@c2.com>
2021-09-16 13:02:41 -0700

    ready for deno deploy

---
+++ server.js
@@ -0,0 +1,48 @@
+// serve and render progress in 6-hour extract cycle
+// usage: ACCT_1_INSIGHTS_QUERY_KEY='...' ~/.deno/bin/deployctl run server.js
+
+let html = await Deno.readFile("./remaining.html")
+let query = `select task, remaining from eldoradoTask where remaining is not null since 24 hours ago limit 400`
+
+addEventListener("fetch", async (event) => {
+  let { pathname, search, origin } = new URL(event.request.url)
+  if (pathname == `/`) {
+    event.respondWith(
+      new Response(html, {
+        status: 200,
+        headers: {
+          "content-type": "text/html; charset=UTF-8",
+        }
+      })
+    )
+  } else if (pathname == `/result.json`) {
+    let result = (await nrql(query))
+    event.respondWith(
+      new Response(JSON.stringify(result,null,2)), {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+        }
+      })
+  } else {
+    event.respondWith(
+      new Response(`can't handle ${event.request.url}`, {
+        status: 500,
+        headers: {
+          "content-type": "text/html",
+        }
+      })
+    )
+  } 
+})
+
+function nrql(query) {
+  let site = `https://staging-insights-api.newrelic.com`
+  let route = `v1/accounts/1/query`
+  let url = `${site}/${route}?nrql=${query}`
+  let headers = {
+    "Accept": "application/json",
+    "X-Query-Key": Deno.env.get("ACCT_1_INSIGHTS_QUERY_KEY")
+  }
+  return fetch(url, {headers}).then(res => res.ok ? res.json() : console.log(res.statusText))
+}

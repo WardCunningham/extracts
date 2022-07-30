5bc589c2b230e1bc7b58f66565229cf749c931ef
Ward Cunningham <ward@c2.com>
2022-07-30 14:10:48 -0700

    parse headers for admin

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ await serve(async request => {
   try {
     let { pathname, search, origin } = new URL(request.url)
     let params = new URLSearchParams(search)
-    let admin = Object.fromEntries(request.headers.entries())
-    console.log('headers', request.headers, admin)
+    let headers = Object.fromEntries(request.headers.entries())
+    console.log('headers', headers)
+    let admin = headers.cookie && headers.cookie.match(/admin=\d/)
+    console.log('admin',admin)
 
     const head = mime => ({"content-type": `${mime}; charset=UTF-8`, "access-control-allow-origin": "*"})
     const resp = (status, headers, body) => new Response(body, {status, headers})
